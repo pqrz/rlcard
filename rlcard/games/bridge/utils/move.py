bc9ab34d98b3bd1f88d747de18bb73154214fe94
pqrz <72381880+pqrz@users.noreply.github.com>
2022-06-29 20:30:39 +0530

    Update move.py

--- rlcard/games/bridge/utils/move.py
+++ rlcard/games/bridge/utils/move.py
@@ -4,8 +4,8 @@
     Date created: 11/25/2021
 '''
 
-#
-#   These classes are used to keep a move_sheet history of the moves in a round.
+# 
+# These classes are used to keep a move_sheet history of the moves in a round.
 #
 
 from .action_event import ActionEvent, BidAction, PassAction, DblAction, RdblAction, PlayCardAction
@@ -14,9 +14,10 @@ from .bridge_card import BridgeCard
 from ..player import BridgePlayer
 
 
+
+# 1. Interface:
 class BridgeMove(object):  # Interface
     pass
-
 
 class PlayerMove(BridgeMove):  # Interface
 
@@ -26,12 +27,8 @@ class PlayerMove(BridgeMove):  # Interface
         self.action = action
 
 
-class CallMove(PlayerMove):  # Interface
-
-    def __init__(self, player: BridgePlayer, action: ActionEvent):
-        super().__init__(player=player, action=action)
-
-
+# 2. Moves:
+## 2.1 Move = ?
 class DealHandMove(BridgeMove):
 
     def __init__(self, dealer: BridgePlayer, shuffled_deck: [BridgeCard]):
@@ -44,6 +41,7 @@ class DealHandMove(BridgeMove):
         return f'{self.dealer} deal shuffled_deck=[{shuffled_deck_text}]'
 
 
+## 2.2 Move = Bid phase / Pass
 class MakePassMove(CallMove):
 
     def __init__(self, player: BridgePlayer):
@@ -53,24 +51,7 @@ class MakePassMove(CallMove):
         return f'{self.player} {self.action}'
 
 
-class MakeDblMove(CallMove):
-
-    def __init__(self, player: BridgePlayer):
-        super().__init__(player=player, action=DblAction())
-
-    def __str__(self):
-        return f'{self.player} {self.action}'
-
-
-class MakeRdblMove(CallMove):
-
-    def __init__(self, player: BridgePlayer):
-        super().__init__(player=player, action=RdblAction())
-
-    def __str__(self):
-        return f'{self.player} {self.action}'
-
-
+## 2.3 Move = Bid phase / Make bid
 class MakeBidMove(CallMove):
 
     def __init__(self, player: BridgePlayer, bid_action: BidAction):
@@ -81,6 +62,7 @@ class MakeBidMove(CallMove):
         return f'{self.player} bids {self.action}'
 
 
+# 2.4: Move = Play Card Phase
 class PlayCardMove(PlayerMove):
 
     def __init__(self, player: BridgePlayer, action: PlayCardAction):
