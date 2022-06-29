c143ba891567f8819d239d01ae2821eaadfc93f9
pqrz <72381880+pqrz@users.noreply.github.com>
2022-06-29 18:44:38 +0530

    Update judger.py

--- rlcard/games/bridge/judger.py
+++ rlcard/games/bridge/judger.py
@@ -15,6 +15,12 @@ from .utils.action_event import ActionEvent, BidAction, PassAction, DblAction, R
 from .utils.move import MakeBidMove, MakeDblMove, MakeRdblMove
 from .utils.bridge_card import BridgeCard
 
+'''
+Returns all possible legal actions for
+    * Bidding phase
+    * Card play phase
+'''
+
 
 class BridgeJudger:
 
@@ -35,28 +41,30 @@ class BridgeJudger:
         legal_actions: List[ActionEvent] = []
         if not self.game.is_over():
             current_player = self.game.round.get_current_player()
+            # 1. If phase = Bidding 
             if not self.game.round.is_bidding_over():
+                
+                # 1.1 Action = Pass
                 legal_actions.append(PassAction())
+                
+                # 1.2 Action = Bidding (but should be greater then last bidding)
+                ##    Steps:
+                ##        1.2.1 Find highest bid till now (e.g.last_make_bid_move = 8 )
                 last_make_bid_move: MakeBidMove or None = None
-                last_dbl_move: MakeDblMove or None = None
-                last_rdbl_move: MakeRdblMove or None = None
                 for move in reversed(self.game.round.move_sheet):
                     if isinstance(move, MakeBidMove):
                         last_make_bid_move = move
                         break
-                    elif isinstance(move, MakeRdblMove):
-                        last_rdbl_move = move
-                    elif isinstance(move, MakeDblMove) and not last_rdbl_move:
-                        last_dbl_move = move
+
                 first_bid_action_id = ActionEvent.first_bid_action_id
+                
+                ##         1.2.2 legal actions = [9, 35]   ............ [last_make_bid_move.action.action_id + 1,   first_bid_action_id + 35]
                 next_bid_action_id = last_make_bid_move.action.action_id + 1 if last_make_bid_move else first_bid_action_id
                 for bid_action_id in range(next_bid_action_id, first_bid_action_id + 35):
                     action = BidAction.from_action_id(action_id=bid_action_id)
                     legal_actions.append(action)
-                if last_make_bid_move and last_make_bid_move.player.player_id % 2 != current_player.player_id % 2 and not last_dbl_move and not last_rdbl_move:
-                    legal_actions.append(DblAction())
-                if last_dbl_move and last_dbl_move.player.player_id % 2 != current_player.player_id % 2:
-                    legal_actions.append(RdblAction())
+
+            # 2. If phase = Card play
             else:
                 trick_moves = self.game.round.get_trick_moves()
                 hand = self.game.round.players[current_player.player_id].hand
