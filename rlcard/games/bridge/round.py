c1f9074f2c086acf7fc1c391124224e9c7a185d3
pqrz <72381880+pqrz@users.noreply.github.com>
2022-06-29 18:32:03 +0530

    Update round.py

--- rlcard/games/bridge/round.py
+++ rlcard/games/bridge/round.py
@@ -15,6 +15,7 @@ from .utils.tray import Tray
 
 
 '''
+Handle a round
 1. __init__() = Init player for round
 2. playCard() = current player plays a card   ->   if he's the last, then compute the winner too
 3. get_current_player(), get_trick_moves() = Info functions for round
